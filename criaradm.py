fb563f5deec9e7e07aec565443e1152e95ac8565
RayFarias13 <1313@gmail.com>
2026-03-09 16:04:21 -0300

    1604 09032026

--- criaradm.py
+++ criaradm.py
@@ -3,16 +3,20 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
+# Configura o ambiente do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitor_api.settings')
 django.setup()
 
 User = get_user_model()
-USERADMIN = os.getenv('DJANGO_SUPERUSER_USERNAME')
-EMAILADMIN = os.getenv('DJANGO_SUPERUSER_EMAIL')
-PASSWORDADMIN = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
-if not User.objects.filter(username=USERADMIN).exists():
-    User.objects.create_superuser(USERADMIN, EMAILADMIN, PASSWORDADMIN)
-    print(f"Superusuário '{USERADMIN}' criado com sucesso!")
+# Pega das variáveis do Render ou usa um padrão caso estejam vazias
+# O segundo argumento é o "plano B" se a variável não existir
+username = os.getenv('USERADMIN')
+email = os.getenv('EMAILADMIN')
+password = os.getenv('PASSWORDADMIN')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Sucesso: Usuário '{username}' criado!")
 else:
-    print(f"Superusuário '{USERADMIN}' já existe.")
+    print(f"Aviso: Usuário '{username}' já existe no Neon.")
