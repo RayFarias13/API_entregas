f87f2ca61521bf18bf42de7b12a29986ef37080e
RayFarias13 <1313ray@gmail.com>
2026-03-24 22:33:29 -0300

    2233

--- software/admin.py
+++ software/admin.py
@@ -19,9 +19,3 @@ class DadoskilometragemAdmin(admin.ModelAdmin):
 
 
 
-@admin.register(EntregaFinalizada)
-class EntregasFinalizadasAdmin(admin.ModelAdmin):
-    list_display = ("id", "cd_entr", "cd_vd", "cd_fun_entr", "cd_fun_lib", "cd_mov_ret", "cd_filial", "data_entr_ini", "hora_entr_ini", "data_hora_atribuicao")
-    search_fields = ("cd_entr", "cd_vd", "cd_fun_entr", "cd_fun_lib")
-    list_filter = ("data_entr_ini",)
-    readonly_fields = ("id",)
