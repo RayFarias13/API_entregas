2457337d131c53f9ce330073174ce28585a6f18b
RayFarias13 <1313@gmail.com>
2026-03-30 13:06:28 -0300

    1306 30032026 alterando dados admin.py

--- software/admin.py
+++ software/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 
-#from software.models import Funcionarios_lista, dadoskilometragem, EntregaFinalizada, dadosvenda, dadosentrega
-'''
+from software.models import Funcionarios_lista, dadoskilometragem, EntregaFinalizada, DadosVenda, DadosEntrega
+''
 # Register your models here.
 
-@admin.register(dadosvenda)
+@admin.register(DadosVenda)
 class DadosVendaAdmin(admin.ModelAdmin):
     list_display = ("id", "cd_cli", "cd_nf", "cd_vd")
     search_fields = ("cd_cli", "cd_nf", "cd_vd")
     readonly_fields = ("id",)
 
-@admin.register(dadosentrega)
+@admin.register(DadosEntrega)
 class DadosEntregaAdmin(admin.ModelAdmin):
     list_display = ("id", "cd_entr", "cd_vd", "cd_fun_entr", "cd_fun_lib", "cd_mov_ret", "cd_filial", "data_entr_ini", "hora_entr_ini", "data_hora_atribuicao")
     search_fields = ("cd_entr", "cd_vd", "cd_fun_entr", "cd_fun_lib", "cd_mov_ret", "cd_filial")
@@ -38,4 +38,4 @@ class CustomerAdmin(admin.ModelAdmin):
     search_fields = ("usermotoboy__username", "destinatario", "endereco_destino")
     list_filter = ("data_entrega",)
 
-'''
+
